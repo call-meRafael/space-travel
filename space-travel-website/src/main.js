62f31659de26d4f462ead8bb10f6f7d8a58f5bfe
countdown-br <raaphael.araujo@gmail.com>
2026-05-15 15:09:10 -0300

    <space-travel> adiciona o menu hamburguer para melhor controle do nav em telas pequenas com função toggle via JS.

--- space-travel-website/src/main.js
+++ space-travel-website/src/main.js
@@ -1,3 +1,39 @@
+'use strict';
+
 // JSON data file
 import { destinations, crew, technology } from './data.json' with { type: "json" };
 
+const nav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+
+
+const openNav = () => {
+    nav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+}
+
+const closeNav = () => {
+    nav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+}
+
+navToggle.addEventListener('click', () => {
+
+    const isVisible = nav.getAttribute('data-visible') === 'true';
+
+    if (isVisible) {
+        closeNav();
+    } else {
+        openNav();
+    }
+})
+
+
+document.addEventListener('click', (e) => {
+    const isVisible = nav.getAttribute("data-visible") === "true";
+
+    if (isVisible && !nav.contains(e.target) && !navToggle.contains(e.target) ) {
+        closeNav();
+    }
+
+})
